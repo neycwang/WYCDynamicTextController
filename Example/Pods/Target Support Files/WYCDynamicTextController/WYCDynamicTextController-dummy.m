412a8743ae90e5e40fe2366e36aee3e479dc0291
apple <apple@appledeMacBook-Air-5.local>
2017-02-19 17:13:24 +0800

    Getting started

---
+++ Example/Pods/Target Support Files/WYCDynamicTextController/WYCDynamicTextController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WYCDynamicTextController : NSObject
+@end
+@implementation PodsDummy_WYCDynamicTextController
+@end
---
+++ Example/WYCDynamicTextController/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WYCDynamicTextController
+//
+//  Created by neycwang@gmail.com on 02/19/2017.
+//  Copyright (c) 2017 neycwang@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
